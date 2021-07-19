8b8f84836acabc5fd3489cbc8f7b63484b70422b
Han Dao <handao@everfit.io>
2021-07-19 17:47:19 -0500

    download as pdf button

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Pdf from "react-to-pdf";
+
+// const ref = React.createRef();
 
 function AnimeCard({ title, url, imageUrl, authorName, intro }) {
+  const ref = React.createRef();
+
   return (
-    <div className="anime-card">
+    <div className="anime-card" ref={ref}>
       <div className="img-container">
         <img alt="preview" src={imageUrl} className="card-img"></img>
       </div>
@@ -11,9 +16,20 @@ function AnimeCard({ title, url, imageUrl, authorName, intro }) {
         <h1 className="card-title">{title}</h1>
         <h5>{authorName}</h5>
         <p>{intro}</p>
+
+        {/* <div className="button-container"> */}
         <a href={url}>
           <button className="read-btn">Read</button>
         </a>
+
+        <Pdf targetRef={ref} filename="animecard.pdf">
+          {({ toPdf }) => (
+            <button onClick={toPdf} className="pdf-btn">
+              Download as PDF
+            </button>
+          )}
+        </Pdf>
+        {/* </div> */}
       </div>
     </div>
   );
